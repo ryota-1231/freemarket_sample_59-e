ad24d8006f42150db65a9fa10991db3f303cbbd6
ryota-1231 <rrrrr.1231@gmail.com>
2019-09-23 10:20:50 +0900

    mastar.key設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url,  'git@github.com:ryota-1231/freemarket_sample_59-e.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
@@ -38,4 +38,16 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
