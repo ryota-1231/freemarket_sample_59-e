7d5b93c37399dab5a61c2541d687887b3874d50c
daiki-toshima <ponkotu2017@gmail.com>
2019-09-26 19:41:13 +0900

    add_top_page2

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -130,15 +130,4 @@ $(function(){
   });
 });
 
-// $(function () {
-//   $('.content__all__scrol').click(function() {
-//     var speed = 400;
-//     var href= $(this).attr("href");
-//     var target = $(href == "#" || href == "" ? 'html' : href);
-//     var position = target.offset().top;
-//     $('body,html').animate({scrollTop:position}, speed, 'swing');
-//     return false;
-//   });
-// });
-
 
