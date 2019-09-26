035355537aa20f4104729e1cf9f196862519f4f0
makun-u <tor_a11um@icloud.com>
2019-09-26 18:37:47 +0900

    login-commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,15 @@ Rails.application.routes.draw do
   resources :items
 
   resources :users
+
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # ここで、入力の全てが終了する
+      get 'done' # 登録完了後のページ
+    end
+  end
+  
 end
