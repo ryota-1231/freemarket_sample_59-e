d891661a62d7b58a42149132d8c7b052ab4247ad
kaito-s <kaito.2019.0802@gmail.com>
2019-09-28 18:41:50 +0900

    bbb

--- db/schema.rb
+++ db/schema.rb
@@ -73,11 +73,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_080835) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "fullname", null: false
-    t.string "kana", null: false
-    t.string "email", null: false
-    t.integer "phone_number", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
