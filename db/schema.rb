4d1be5ba889f326a3250b702ed8abab29c200f03
ryota-1231 <rrrrr.1231@gmail.com>
2019-09-28 18:37:59 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_28_080835) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode", null: false
@@ -75,7 +73,11 @@ ActiveRecord::Schema.define(version: 2019_09_28_080835) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "nickname", null: false
+    t.string "fullname", null: false
+    t.string "kana", null: false
+    t.string "email", null: false
+    t.integer "phone_number", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
