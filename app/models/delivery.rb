1b135de5a80013482d57ccce31af2d4c6873ec4a
ryota-1231 <rrrrr.1231@gmail.com>
2019-09-29 16:50:17 +0900

    active_hashファイル作成

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,6 @@
 class Delivery < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :deliveryfee, :deliverydate, :deliverymethod
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_104515) do
+ActiveRecord::Schema.define(version: 2019_09_29_043334) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode", null: false
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2019_09_28_104515) do
     t.text "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "ancestry"
   end
 
   create_table "buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
