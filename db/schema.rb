c530aeed6e3a89ed2f1ea15dadea19344953e624
daiki-toshima <ponkotu2017@gmail.com>
2019-10-03 12:00:49 +0900

    ええええ

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_10_02_093550) do
+ActiveRecord::Schema.define(version: 2019_09_29_154535) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode", null: false
