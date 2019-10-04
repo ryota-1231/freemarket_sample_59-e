496981a59db074fe872835479190e32c95cad259
ryota-1231 <rrrrr.1231@gmail.com>
2019-10-04 13:13:05 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,8 @@ set :application, 'freemarket_sample_59-e'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:ryota-1231/freemarket_sample_59-e.git'
 
+ask(:branch, 'master')
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
