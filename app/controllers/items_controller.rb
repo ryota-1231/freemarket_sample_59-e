ded34dc80a95cf083f4d76b360b947a247533599
kaito-s <kaito.2019.0802@gmail.com>
2019-10-06 12:19:27 +0900

    free

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,6 @@ class ItemsController < ApplicationController
     #ログイン機能実装前なのでコメントアウトしてあります
 
     # if @item.user_id == current_user.id
-      binding.pry
       if items_params[:sizetype_id]
         @item.update(items_params)
       else
