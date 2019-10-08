9383575fc40755bcb405e7b44fadaeb64f4ef5be
kaito-s <kaito.2019.0802@gmail.com>
2019-10-08 13:22:06 +0900

    add_good_ajax
    いいね機能の非同期化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,9 @@
 class CommentsController < ApplicationController
-  # def new
-  #   @item = Item.find(params[:item_id])
-  #   @comment = Comment.new
-  # end
 
   def create
     @item = Item.find(params[:item_id])
-    @comment = @item.comments.new(create_params)
-    if  @comment.save
-        redirect_to item_path(@item)
-    end
+    @comment = @item.comments.create(create_params)
+    redirect_to item_path(@item)
   end
 
   private
