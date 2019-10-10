1e92250ea4b4269406b84e40d08697bc1725f8bc
ryota-1231 <rrrrr.1231@gmail.com>
2019-10-10 17:29:02 +0900

    データベース名前変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -198,15 +198,15 @@ require "csv"
 
 require "csv"
 
-# CSV.foreach('db/sizetypes.csv', headers: true) do |row|
-#     Sizetype.create(
-#     # id: row['id'],
-#     sizetype: row['sizetype'],
-#     created_at: row['created_at'],
-#     updated_at: row['updated_at'],
-#     ancestry: row['ancestry']
-#   )
-#   end
+CSV.foreach('db/sizetypes.csv', headers: true) do |row|
+    Sizetype.create(
+    # id: row['id'],
+    sizetype: row['sizetype'],
+    created_at: row['created_at'],
+    updated_at: row['updated_at'],
+    ancestry: row['ancestry']
+  )
+  end
 
 CSV.foreach('db/brands.csv', headers: true) do |row|
     Brand.create(
@@ -217,18 +217,18 @@ CSV.foreach('db/brands.csv', headers: true) do |row|
     )
 end
 
-CSV.foreach('db/categories.csv', headers: true) do |row|
-    Category.create(
-    # id: row['id'],
-    name: row['name'],
-    created_at: row['created_at'],
-    updated_at: row['updated_at'],
-    ancestry: row['ancestry'],
-    sizetype_id: row['sizetype_id'],
-    title: row['title'],
-    text: row['text']
-    )
-end
+# CSV.foreach('db/categories.csv', headers: true) do |row|
+#     Category.create(
+#     # id: row['id'],
+#     name: row['name'],
+#     created_at: row['created_at'],
+#     updated_at: row['updated_at'],
+#     ancestry: row['ancestry'],
+#     sizetype_id: row['sizetype_id'],
+#     title: row['title'],
+#     text: row['text']
+#     )
+# end
 
 
 
