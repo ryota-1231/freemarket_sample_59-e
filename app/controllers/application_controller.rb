6833d8c8ba912ea3ba4fcf82ee229c6a1a791421
ryota-1231 <rrrrr.1231@gmail.com>
2019-10-20 14:59:20 +0900

    basic認証訂正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production? 
+  before_action :basic_auth
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
- 
 
   private
-
-  def production?
-    Rails.env.production?
-  end
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
+      username == Rails.application.credentials[:basic_user_name] && password == Rails.application.credentials[:basic_password]
+    end   
   end
 
   protected
