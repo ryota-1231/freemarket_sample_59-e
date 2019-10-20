77eac235fb05c05176c09a4e0eb2ad75f75cd545
ryota-1231 <rrrrr.1231@gmail.com>
2019-10-20 14:42:19 +0900

    basic認証変数設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production? 
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+ 
 
   private
+
+  def production?
+    Rails.env.production?
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials.basic_user_name && password == Rails.application.credentials.basic_password
-    end   
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 
   protected
